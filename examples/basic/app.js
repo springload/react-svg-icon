45596912633b29fe531bba8dddae4d8f9d9af9f4
Vincent Audebert <vincent@springload.co.nz>
2015-10-29 17:36:35 +1300

    adding some tests + upgrading rackt-cli to a working version :-/

--- examples/basic/app.js
+++ examples/basic/app.js
@@ -2,15 +2,15 @@ import React from 'react';
 import Icon from '../../lib/index';
 import ReactDOM from 'react-dom';
 
-let App = React.createClass({
-    render() {
-      return (
-          <div className="example">
-              <h1>React SVG icon example</h1>
-              <Icon name="rocket" className="i--red" />
-          </div>
-      );
-    }
+const App = React.createClass({
+  render() {
+    return (
+      <div className="example">
+        <h1>React SVG icon example</h1>
+        <Icon name="rocket" className="i--red" />
+      </div>
+    );
+  }
 });
 
 ReactDOM.render(<App/>, document.getElementById('container'));
