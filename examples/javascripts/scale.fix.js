f13a8f0ae1d075c58d5ac0608fdc612046a6e106
Vincent Audebert <vincent.audebert@yahoo.fr>
2015-11-02 16:35:14 +1300

    Fixing issues for tests.

--- examples/javascripts/scale.fix.js
+++ examples/javascripts/scale.fix.js
@@ -1,17 +1,19 @@
-var metas = document.getElementsByTagName('meta');
-var i;
-if (navigator.userAgent.match(/iPhone/i)) {
-  for (i=0; i<metas.length; i++) {
-    if (metas[i].name == "viewport") {
-      metas[i].content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
-    }
-  }
-  document.addEventListener("gesturestart", gestureStart, false);
-}
+const metas = document.getElementsByTagName('meta');
+let i;
+
 function gestureStart() {
-  for (i=0; i<metas.length; i++) {
-    if (metas[i].name == "viewport") {
-      metas[i].content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6";
+  for (i = 0; i < metas.length; i++) {
+    if (metas[i].name === 'viewport') {
+      metas[i].content = 'width=device-width, minimum-scale=0.25, maximum-scale=1.6';
     }
   }
+}
+
+if (navigator.userAgent.match(/iPhone/i)) {
+  for (i = 0; i < metas.length; i++) {
+    if (metas[i].name === 'viewport') {
+      metas[i].content = 'width=device-width, minimum-scale=1.0, maximum-scale=1.0';
+    }
+  }
+  document.addEventListener('gesturestart', gestureStart, false);
 }
